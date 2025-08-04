55affeecb6a7e7345840edc27292ddb7bbd92d5e
Ed Holmes <ed.holmes@me.com>
2025-08-04 20:06:16 +1000

    usbd formatting

--- USB_Device/App/usbd_custom_hid_if.c
+++ USB_Device/App/usbd_custom_hid_if.c
@@ -35,62 +35,64 @@
 /* USER CODE END PV */
 
 /** @addtogroup STM32_USB_OTG_DEVICE_LIBRARY
-  * @brief Usb device.
-  * @{
-  */
+ * @brief Usb device.
+ * @{
+ */
 
 /** @addtogroup USBD_CUSTOM_HID
-  * @{
-  */
+ * @{
+ */
 
 /** @defgroup USBD_CUSTOM_HID_Private_TypesDefinitions USBD_CUSTOM_HID_Private_TypesDefinitions
-  * @brief Private types.
-  * @{
-  */
+ * @brief Private types.
+ * @{
+ */
 
 /* USER CODE BEGIN PRIVATE_TYPES */
 
 /* USER CODE END PRIVATE_TYPES */
 
 /**
-  * @}
-  */
+ * @}
+ */
 
 /** @defgroup USBD_CUSTOM_HID_Private_Defines USBD_CUSTOM_HID_Private_Defines
-  * @brief Private defines.
-  * @{
-  */
+ * @brief Private defines.
+ * @{
+ */
 
 /* USER CODE BEGIN PRIVATE_DEFINES */
 #define DESC_1
 /* USER CODE END PRIVATE_DEFINES */
 
 /**
-  * @}
-  */
+ * @}
+ */
 
 /** @defgroup USBD_CUSTOM_HID_Private_Macros USBD_CUSTOM_HID_Private_Macros
-  * @brief Private macros.
-  * @{
-  */
+ * @brief Private macros.
+ * @{
+ */
 
 /* USER CODE BEGIN PRIVATE_MACRO */
 
 /* USER CODE END PRIVATE_MACRO */
 
 /**
-  * @}
-  */
+ * @}
+ */
 
 /** @defgroup USBD_CUSTOM_HID_Private_Variables USBD_CUSTOM_HID_Private_Variables
-  * @brief Private variables.
-  * @{
-  */
+ * @brief Private variables.
+ * @{
+ */
 
 /** Usb HID report descriptor. */
-__ALIGN_BEGIN static uint8_t CUSTOM_HID_ReportDesc_FS[USBD_CUSTOM_HID_REPORT_DESC_SIZE] __ALIGN_END =
-{
-  /* USER CODE BEGIN 0 */
+__ALIGN_BEGIN static uint8_t CUSTOM_HID_ReportDesc_FS[USBD_CUSTOM_HID_REPORT_DESC_SIZE] __ALIGN_END = {
+/* USER CODE BEGIN 0 */
+// clang-format off
+
+
 // 0x05, 0x0D,       // Usage Page (Digitizer)
 // 0x09, 0x04,       // Usage (Touch Screen)
 // 0xA1, 0x01,       // Collection (Application)
@@ -157,84 +159,45 @@ __ALIGN_BEGIN static uint8_t CUSTOM_HID_ReportDesc_FS[USBD_CUSTOM_HID_REPORT_DES
 // 0x00,
 #ifdef DESC_1
     // TOUCHSCRREN = 0
-    0x05,
-    0x01, // USAGE_PAGE (Generic Desktop)
-    0x09,
-    0x01, // USAGE (Pointer)
-    0xa1,
-    0x01, // COLLECTION (Application)
+    0x05, 0x01,        // Usage Page (Generic Desktop Ctrls)
+    0x09, 0x02,        // Usage (Mouse)
+    0xA1, 0x01,        // Collection (Application)
+    0x85, 0x01,        //   Report ID (1)
+    0x09, 0x01,        //   Usage (Pointer)
+    0xA1, 0x00,        //   Collection (Physical)
+    // Buttons (3 bits)
+    0x05, 0x09,        //     Usage Page (Button)
+    0x19, 0x01,        //     Usage Minimum (0x01)
+    0x29, 0x03,        //     Usage Maximum (0x03)
+    0x15, 0x00,        //     Logical Minimum (0)
+    0x25, 0x01,        //     Logical Maximum (1)
+    0x95, 0x03,        //     Report Count (3)
+    0x75, 0x01,        //     Report Size (1)
+    0x81, 0x02,        //     Input (Data,Var,Abs)
+    // Padding (5 bits)
+    0x95, 0x01,        //     Report Count (1)
+    0x75, 0x05,        //     Report Size (5)
+    0x81, 0x03,        //     Input (Const,Var,Abs)
+    // X and Y Coordinates (16-bit absolute)
+    0x05, 0x01,        //     Usage Page (Generic Desktop Ctrls)
+    0x09, 0x30,        //     Usage (X)
+    0x09, 0x31,        //     Usage (Y)
+    0x16, 0x01, 0x80,  //     Logical Minimum (-32767)
+    0x26, 0xFF, 0x7F,  //     Logical Maximum (32767)
+    0x75, 0x10,        //     Report Size (16)
+    0x95, 0x02,        //     Report Count (2)
+    0x81, 0x02,        //     Input (Data,Var,Abs)
+    // Vertical Wheel
+    0x09, 0x38,        //     Usage (Wheel)
+    0x15, 0x81,        //     Logical Minimum (-127)
+    0x25, 0x7F,        //     Logical Maximum (127)
+    0x75, 0x08,        //     Report Size (8)
+    0x95, 0x01,        //     Report Count (1)
+    0x81, 0x06,        //     Input (Data,Var,Rel)
+    0xC0,              //   End Collection
 
-    // declare a pointer collection
-    0x05,
-    0x01, //   USAGE_PAGE (Generic Desktop)
-    0x09,
-    0x01, //   USAGE (Pointer)
-    0xa1,
-    0x00, //   COLLECTION (Physical)
 
-    // Declare a button
-    0x05,
-    0x09, //     USAGE_PAGE (Button)
-    0x19,
-    0x01, //     USAGE_MINIMUM (Button 1)
-    0x29,
-    0x01, //     USAGE_MAXIMUM (Button 1)
-    0x15,
-    0x00, //     LOGICAL_MINIMUM (0)
-    0x25,
-    0x01, //     LOGICAL_MAXIMUM (1)
-    0x35,
-    0x00, //     PHYSICAL_MINIMUM (0)
-    0x45,
-    0x01, //     PHYSICAL_MAXIMUM (1)
-    0x66,
-    0x00,
-    0x00, //     UNIT (None)
-    0x75,
-    0x01, //     REPORT_SIZE (1)
-    0x95,
-    0x01, //     REPORT_COUNT (1)
-    0x81,
-    0x62, //     INPUT (Data,Var,Abs)
 
-    // Declare the remaining 7 bits of the first data byte as constant -> the driver will ignore them
-    0x75,
-    0x01, //     REPORT_SIZE (1)
-    0x95,
-    0x07, //     REPORT_COUNT (7)
-    0x81,
-    0x01, //     INPUT (Cnst,Ary,Abs)
-
-    // Define absolute X and Y coordinates of 16 bit each (percent values multiplied with 100)
-    0x05,
-    0x01, //     Usage Page (Generic Desktop)
-    0x09,
-    0x30, //     Usage (X)
-    0x09,
-    0x31, //     Usage (Y)
-    0x16,
-    0x00,
-    0x00, //     Logical Minimum (0)
-    0x26,
-    0x10,
-    0x27, //     Logical Maximum (10000)
-    0x36,
-    0x00,
-    0x00, //     Physical Minimum (0)
-    0x46,
-    0x10,
-    0x27, //     Physical Maximum (10000)
-    0x66,
-    0x00,
-    0x00, //     UNIT (None)
-    0x75,
-    0x10, //     Report Size (16),
-    0x95,
-    0x02, //     Report Count (2),
-    0x81,
-    0x62, //     Input (Data,Var,Abs)
-
-    0xc0, //   END_COLLECTION
 #elif defined DESC_2
     // TOCUHSCREEN = 1
 
@@ -315,8 +278,11 @@ __ALIGN_BEGIN static uint8_t CUSTOM_HID_ReportDesc_FS[USBD_CUSTOM_HID_REPORT_DES
     0xc0, //   END_COLLECTION
 
 #endif
-  /* USER CODE END 0 */
-  0xC0    /*     END_COLLECTION	             */
+
+    // clang-format on
+
+    /* USER CODE END 0 */
+    0xC0 /*     END_COLLECTION	             */
 };
 
 /* USER CODE BEGIN PRIVATE_VARIABLES */
@@ -324,88 +290,83 @@ __ALIGN_BEGIN static uint8_t CUSTOM_HID_ReportDesc_FS[USBD_CUSTOM_HID_REPORT_DES
 /* USER CODE END PRIVATE_VARIABLES */
 
 /**
-  * @}
-  */
+ * @}
+ */
 
 /** @defgroup USBD_CUSTOM_HID_Exported_Variables USBD_CUSTOM_HID_Exported_Variables
-  * @brief Public variables.
-  * @{
-  */
+ * @brief Public variables.
+ * @{
+ */
 extern USBD_HandleTypeDef hUsbDeviceFS;
 
 /* USER CODE BEGIN EXPORTED_VARIABLES */
 
 /* USER CODE END EXPORTED_VARIABLES */
 /**
-  * @}
-  */
+ * @}
+ */
 
 /** @defgroup USBD_CUSTOM_HID_Private_FunctionPrototypes USBD_CUSTOM_HID_Private_FunctionPrototypes
-  * @brief Private functions declaration.
-  * @{
-  */
+ * @brief Private functions declaration.
+ * @{
+ */
 
 static int8_t CUSTOM_HID_Init_FS(void);
 static int8_t CUSTOM_HID_DeInit_FS(void);
 static int8_t CUSTOM_HID_OutEvent_FS(uint8_t event_idx, uint8_t state);
 
 /**
-  * @}
-  */
+ * @}
+ */
 
-USBD_CUSTOM_HID_ItfTypeDef USBD_CustomHID_fops_FS =
-{
-  CUSTOM_HID_ReportDesc_FS,
-  CUSTOM_HID_Init_FS,
-  CUSTOM_HID_DeInit_FS,
-  CUSTOM_HID_OutEvent_FS
-};
+USBD_CUSTOM_HID_ItfTypeDef USBD_CustomHID_fops_FS = {
+    CUSTOM_HID_ReportDesc_FS, CUSTOM_HID_Init_FS, CUSTOM_HID_DeInit_FS, CUSTOM_HID_OutEvent_FS};
 
 /** @defgroup USBD_CUSTOM_HID_Private_Functions USBD_CUSTOM_HID_Private_Functions
-  * @brief Private functions.
-  * @{
-  */
+ * @brief Private functions.
+ * @{
+ */
 
 /* Private functions ---------------------------------------------------------*/
 
 /**
-  * @brief  Initializes the CUSTOM HID media low layer
-  * @retval USBD_OK if all operations are OK else USBD_FAIL
-  */
+ * @brief  Initializes the CUSTOM HID media low layer
+ * @retval USBD_OK if all operations are OK else USBD_FAIL
+ */
 static int8_t CUSTOM_HID_Init_FS(void)
 {
   /* USER CODE BEGIN 4 */
-    return (USBD_OK);
+  return (USBD_OK);
   /* USER CODE END 4 */
 }
 
 /**
-  * @brief  DeInitializes the CUSTOM HID media low layer
-  * @retval USBD_OK if all operations are OK else USBD_FAIL
-  */
+ * @brief  DeInitializes the CUSTOM HID media low layer
+ * @retval USBD_OK if all operations are OK else USBD_FAIL
+ */
 static int8_t CUSTOM_HID_DeInit_FS(void)
 {
   /* USER CODE BEGIN 5 */
-    return (USBD_OK);
+  return (USBD_OK);
   /* USER CODE END 5 */
 }
 
 /**
-  * @brief  Manage the CUSTOM HID class events
-  * @param  event_idx: Event index
-  * @param  state: Event state
-  * @retval USBD_OK if all operations are OK else USBD_FAIL
-  */
+ * @brief  Manage the CUSTOM HID class events
+ * @param  event_idx: Event index
+ * @param  state: Event state
+ * @retval USBD_OK if all operations are OK else USBD_FAIL
+ */
 static int8_t CUSTOM_HID_OutEvent_FS(uint8_t event_idx, uint8_t state)
 {
   /* USER CODE BEGIN 6 */
-    UNUSED(event_idx);
-    UNUSED(state);
+  UNUSED(event_idx);
+  UNUSED(state);
 
-    /* Start next USB packet transfer once data processing is completed */
-    USBD_CUSTOM_HID_ReceivePacket(&hUsbDeviceFS);
+  /* Start next USB packet transfer once data processing is completed */
+  USBD_CUSTOM_HID_ReceivePacket(&hUsbDeviceFS);
 
-    return (USBD_OK);
+  return (USBD_OK);
   /* USER CODE END 6 */
 }
 
@@ -428,14 +389,13 @@ static int8_t USBD_CUSTOM_HID_SendReport_FS(uint8_t *report, uint16_t len)
 
 /* USER CODE END PRIVATE_FUNCTIONS_IMPLEMENTATION */
 /**
-  * @}
-  */
+ * @}
+ */
 
 /**
-  * @}
-  */
+ * @}
+ */
 
 /**
-  * @}
-  */
-
+ * @}
+ */
