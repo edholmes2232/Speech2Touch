8189e9a844f00c000bc5eeb32e080e6d0d604fc6
Ed Holmes <ed.holmes@me.com>
2025-08-08 21:42:39 +1000

    Qt with animation

--- Core/Inc/touch_targets.h
+++ Core/Inc/touch_targets.h
@@ -1,6 +1,13 @@
 #ifndef TOUCH_TARGETS_H
 #define TOUCH_TARGETS_H
 
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 typedef enum
 {
   // Page 0
@@ -32,6 +39,8 @@ typedef enum
   // Page 2
   TARGET_WARM_MILK_FOAM,
   TARGET_COLD_MILK_FOAM,
+
+  TARGET_COUNT,
 } TARGET_T;
 
 typedef struct
@@ -39,37 +48,13 @@ typedef struct
   uint8_t page;
   uint8_t x;
   uint8_t y;
+  char name[24];
 } TOUCH_TARGET_T;
 
-static TOUCH_TARGET_T touch_targets[] = {
-    // clang-format off
-    /* TARGET_ESPRESSO */           { 0, 0, 0 }, // page 0 [0,0]
-    /* TARGET_DOUBLE_ESPRESSO */    { 0, 1, 0 }, // page 0 [1,0]
-    /* TARGET_ESPRESSO_MACCHIATO */ { 0, 2, 0 }, // page 0 [2,0]
-    /* TARGET_LONG_BLACK */         { 0, 3, 0 }, // page 0 [3,0]
-    /* TARGET_CAFFE_LATTE */        { 0, 0, 1 }, // page 0 [0,1]
-    /* TARGET_CAPPUCCINO */         { 0, 1, 1 }, // page 0 [1,1]
-    /* TARGET_FLAT_WHITE */         { 0, 2, 1 }, // page 0 [2,1]
-    /* TARGET_MAGIC */              { 0, 3, 1 }, // page 0 [3,1]
-    /* TARGET_PICCOLO_LATTE */      { 0, 0, 2 }, // page 0 [0,2]
-    /* TARGET_HOT_CHOCOLATE */      { 0, 1, 2 }, // page 0 [1,2]
-    /* TARGET_CHAI_LATTE */         { 0, 2, 2 }, // page 0 [2,2]
-    /* TARGET_MOCHA */              { 0, 3, 2 }, // page 0 [3,2]
-    /* TARGET_DIRTY_CHAI */         { 1, 0, 0 }, // page 1 [0,0]
-    /* TARGET_STRONG_LATTE */       { 1, 1, 0 }, // page 1 [1,0]
-    /* TARGET_STRONG_CAPPUCCINO */  { 1, 2, 0 }, // page 1 [2,0]
-    /* TARGET_HOT_MILK */           { 1, 3, 0 }, // page 1 [3,0]
-    /* TARGET_ICED_COFFEE */        { 1, 0, 1 }, // page 1 [0,1]
-    /* TARGET_WEAK_LATTE */         { 1, 1, 1 }, // page 1 [1,1]
-    /* TARGET_WEAK_CAPPUCCINO */    { 1, 2, 1 }, // page 1 [2,1]
-    /* TARGET_HOT_MILK_FOAM */      { 1, 3, 1 }, // page 1 [3,1]
-    /* TARGET_ICED_CHOCOLATE */     { 1, 0, 2 }, // page 1 [0,2]
-    /* TARGET_HOT_LATTE */          { 1, 1, 2 }, // page 1 [1,2]
-    /* TARGET_HOT_WATER */          { 1, 2, 2 }, // page 1 [2,2]
-    /* TARGET_COLD_MILK */          { 1, 3, 2 }, // page 1 [3,2]
-    /* TARGET_WARM_MILK_FOAM */     { 2, 0, 0 }, // page 2 [0,0]
-    /* TARGET_COLD_MILK_FOAM */     { 2, 1, 0 }, // page 2 [1,0]
-    // clang-format on
-};
+extern const TOUCH_TARGET_T touch_targets[];
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
