9f3a76f9e886c340a1eb303971f8134b0456cb45
Ed Holmes <ed.holmes@me.com>
2025-08-18 17:34:36 +1000

    Modify audio to use RTOS buffers

--- Core/Inc/audio.h
+++ Core/Inc/audio.h
@@ -4,7 +4,34 @@
 #include <stdint.h>
 #include <stdio.h>
 
+/**
+ * @brief Initialize the audio subsystem.
+ *
+ * @param memory_ptr Pointer to the memory to be used for audio queue
+ * @note Allocates it's own memory for audio buffers separately.
+ * @return uint8_t Status of the initialization (EXIT_SUCCESS or EXIT_FAILURE).
+ */
+uint8_t AUDIO_Init(void *memory_ptr);
+
+/**
+ * @brief Start the audio processing.
+ */
 void AUDIO_Start(void);
-int16_t *AUDIO_GetBuffer(void);
+
+/**
+ * @brief Get an audio buffer for processing. Blocks until a buffer is available.
+ *
+ * @param buffer Pointer to pointer to audio buffer
+ * @note The buffer must be released using AUDIO_ReleaseBuffer after processing.
+ * @return uint8_t Status of the operation (EXIT_SUCCESS or EXIT_FAILURE).
+ */
+uint8_t AUDIO_GetBuffer(int16_t **buffer);
+
+/**
+ * @brief Release an audio buffer back to the pool.
+ *
+ * @param buffer Pointer to the audio buffer to be released
+ */
+void AUDIO_ReleaseBuffer(int16_t *buffer);
 
 #endif
--- Core/Src/audio.c
+++ Core/Src/audio.c
@@ -1,16 +1,17 @@
 #include "audio.h"
 
+#include "log.h"
 #include "picovoice.h"
 #include "sai.h"
 #include "stm32wbxx_hal_sai.h"
+#include "tx_api.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 
 #define PV_AUDIO_REC_AUDIO_FREQUENCY (16000U)
 #define PICOVOICE_FRAME_SIZE (512)
-
-volatile int16_t _ping_pong_buffer[2][PICOVOICE_FRAME_SIZE];
+#define AUDIO_BUFFER_SIZE_BYTES (PICOVOICE_FRAME_SIZE * sizeof(int16_t))
 
 static int32_t _dma_buffer[PICOVOICE_FRAME_SIZE * 2];
 
@@ -19,28 +20,65 @@ static volatile int32_t write_index = 0;
 static volatile int32_t read_index = -1;
 static int32_t last_read_index = -1;
 
+// ---------------------------- Azure RTOS Config --------------------------- //
+#define NUM_AUDIO_BUFFERS (4)
+
+// Audio byte pool
+static TX_BLOCK_POOL _audio_block_pool;
+static UCHAR _block_pool_buffer[NUM_AUDIO_BUFFERS * AUDIO_BUFFER_SIZE_BYTES];
+
+// Queue containing int16_t* audio buffer addresses
+static TX_QUEUE _audio_data_queue;
+
+uint8_t AUDIO_Init(VOID *memory_ptr)
+{
+  UINT status;
+  // Create audio block pool
+  status = tx_block_pool_create(
+      &_audio_block_pool, "Audio Buffer Pool", AUDIO_BUFFER_SIZE_BYTES, _block_pool_buffer, sizeof(_block_pool_buffer));
+  if (status != TX_SUCCESS)
+  {
+    log_fatal("Failed to create audio block pool: %d", status);
+    return EXIT_FAILURE;
+  }
+
+  // Create audio data queue storage from global byte pool
+  TX_BYTE_POOL *byte_pool = (TX_BYTE_POOL *)memory_ptr;
+  CHAR *queue_storage_ptr;
+  status = tx_byte_allocate(byte_pool, (VOID **)&queue_storage_ptr, NUM_AUDIO_BUFFERS * sizeof(int16_t *), TX_NO_WAIT);
+  if (status != TX_SUCCESS)
+  {
+    log_fatal("Failed to allocate queue storage: %d", status);
+    return EXIT_FAILURE;
+  }
+
+  // Create audio data queue
+  status = tx_queue_create(&_audio_data_queue,
+                           "Audio Data Queue",
+                           sizeof(int16_t *) / sizeof(ULONG),
+                           queue_storage_ptr,
+                           NUM_AUDIO_BUFFERS * sizeof(int16_t *));
+  if (status != TX_SUCCESS)
+  {
+    log_fatal("Failed to create audio data queue: %d", status);
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
+
 void AUDIO_Start(void)
 {
-  printf("AUDIO_Start\n");
+  log_info("AUDIO_Start");
   // Enable DWT cycle counter for timing if not already enabled
   if (!(DWT->CTRL & DWT_CTRL_CYCCNTENA_Msk))
   {
+    // log_fatal("Enabling DWT cycle counter");
     CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
     DWT->CYCCNT = 0;
     DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
   }
   HAL_SAI_Receive_DMA(&hsai_BlockA1, (uint8_t *)_dma_buffer, PICOVOICE_FRAME_SIZE * 2);
-}
-
-int16_t *AUDIO_GetBuffer(void)
-{
-  if ((read_index == -1) || (last_read_index == read_index))
-  {
-    // No new data available
-    return NULL;
-  }
-  last_read_index = read_index;
-  return _ping_pong_buffer[read_index];
 }
 
 // A helper macro for clamping
@@ -56,7 +94,7 @@ static void processData(const int32_t *dma_src, int16_t *dest)
 
     // 2. Apply a gain factor. Start with 4.
     // (This is a 12dB gain. Use 2 for 6dB, 8 for 18dB, etc.)
-    sample = sample * 4;
+    sample = sample * 8;
 
     // 3. IMPORTANT: Clamp the value to prevent overflow distortion.
     // The raw 24-bit data is in a 32-bit container, so we clamp to the 32-bit min/max.
@@ -68,23 +106,66 @@ static void processData(const int32_t *dma_src, int16_t *dest)
   uint32_t end = DWT->CYCCNT;
   uint32_t cycles = end - start;
   uint32_t us = cycles / (SystemCoreClock / 1000000);
-  printf("processData took %lu us\n", us);
+  // log_trace("processData took %lu us", us);
 }
 
-void HAL_SAI_RxHalfCpltCallback(SAI_HandleTypeDef *hsai)
+void dmaCallbackHandler(int32_t *dma_buffer)
 {
-  processData(&_dma_buffer[0], _ping_pong_buffer[write_index]);
+  // Allocate buff from byte pool
+  int16_t *buffer = NULL;
+  UINT status = tx_block_allocate(&_audio_block_pool, (VOID **)&buffer, TX_NO_WAIT);
+  if ((status != TX_SUCCESS) || (buffer == NULL))
+  {
+    log_error("Failed to allocate audio buffer from byte pool, dropping frame");
+    return;
+  }
 
-  read_index = write_index;
+  processData(_dma_buffer, buffer);
 
-  write_index ^= 1; // Toggle between 0 and 1
+  // Send the buffer to the queue
+  status = tx_queue_send(&_audio_data_queue, &buffer, TX_NO_WAIT);
+  if (status != TX_SUCCESS)
+  {
+    log_error("Failed to send audio buffer to queue, dropping frame");
+    tx_byte_release(buffer);
+
+    return;
+  }
 }
 
 void HAL_SAI_RxCpltCallback(SAI_HandleTypeDef *hsai)
 {
-  processData(&_dma_buffer[PICOVOICE_FRAME_SIZE], _ping_pong_buffer[write_index]);
+  (void)hsai;
+  dmaCallbackHandler(&_dma_buffer[0]);
+}
 
-  read_index = write_index;
+void HAL_SAI_RxHalfCpltCallback(SAI_HandleTypeDef *hsai)
+{
+  (void)hsai;
+  dmaCallbackHandler(&_dma_buffer[PICOVOICE_FRAME_SIZE]);
+}
 
-  write_index ^= 1; // Toggle between 0 and 1
+uint8_t AUDIO_GetBuffer(int16_t **buffer)
+{
+  UINT status = tx_queue_receive(&_audio_data_queue, buffer, TX_WAIT_FOREVER);
+  if (status != TX_SUCCESS)
+  {
+    log_error("Failed to receive audio buffer from queue: %d", status);
+    return EXIT_FAILURE;
+  }
+
+  // log_trace("buffer=%p", *buffer);
+  return EXIT_SUCCESS;
+}
+
+void AUDIO_ReleaseBuffer(int16_t *buffer)
+{
+  if (buffer != NULL)
+  {
+    UINT status = tx_block_release(buffer);
+    if (status != TX_SUCCESS)
+    {
+      log_error("Failed to release audio buffer back to byte pool");
+    }
+  }
 }
